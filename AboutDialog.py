7d93da5f5101d179f1f83bf9ec0206e0786cc928
packituz <pacohm94@gmail.com>
2018-10-05 21:17:12 -0500

    Flake8 Corrections.

--- AboutDialog.py
+++ AboutDialog.py
@@ -3,6 +3,7 @@ from PyQt4.Qt import PYQT_VERSION_STR
 import DialogAbout
 import sys
 
+
 class AboutDialog(DialogAbout.Ui_DialogAbout, QDialog):
     def __init__(self, parent=None, version=''):
         super(AboutDialog, self).__init__(parent)
@@ -11,4 +12,5 @@ class AboutDialog(DialogAbout.Ui_DialogAbout, QDialog):
         python_version = sys.version.split()[0]
 
         self.label_version.setText(' '.join(['Version:', version]))
-        self.label_qt_python.setText(' '.join(['PyQt', PYQT_VERSION_STR, ' Python', python_version]))
+        self.label_qt_python.setText(' '.join(['PyQt', PYQT_VERSION_STR,
+                                               ' Python', python_version]))
--- Settings.py
+++ Settings.py
@@ -38,9 +38,14 @@ class Settings(DialogSettings.Ui_Dialog, QDialog):
         self.callback()
 
     def reset_to_defaults(self):
-        settings_defaults = {'width': 2, 'color': 'blue', 'x_grid': True, 'y_grid': True, 'grid_opacity': 0.5}
+        settings_defaults = {'width': 2,
+                             'color': 'blue',
+                             'x_grid': True,
+                             'y_grid': True,
+                             'grid_opacity': 0.5}
 
-        self.combobox_text_color.setCurrentIndex(self.combobox_text_color.findText(settings_defaults.get('color')))
+        index = self.combobox_text_color.findText(settings_defaults.get('color'))
+        self.combobox_text_color.setCurrentIndex(index)
         self.checkbox_x.setChecked(settings_defaults.get('x_grid'))
         self.checkbox_y.setChecked(settings_defaults.get('y_grid'))
         self.double_spinbox_opacity.setValue(settings_defaults.get('grid_opacity'))
